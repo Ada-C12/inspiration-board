0524288d524d5d41d5655aa1cac5f10a77c3bebf
Nicky Choi <nickyjinchoi@gmail.com>
2019-12-12 18:59:44 -0800

    Returned props  with class names that corresponds with CSS

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,16 +6,23 @@ import './Card.css';
 
 class Card extends Component {
   render() {
+    let displayEmoji = (this.props.emoji) ? emoji.getUnicode(this.props.emoji) : '';
     return (
       <div className="card">
-        Card
+        <section className="card__content">
+          <p className="card__content-text">{this.props.id}</p>
+          <p className="card__content-text">{this.props.text}</p>
+          <p className="card__content-emoji">{displayEmoji}</p>
+        </section>
       </div>
     )
   }
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string,
+  emoji: PropTypes.string,
+  id: PropTypes.number,
 };
 
 export default Card;
