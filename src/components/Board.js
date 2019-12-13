eb57905e931a29aa1eb7897da8c26ffd175fd862
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-12-13 11:28:02 -0800

    added axios get to Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
-// import axios from 'axios';
+import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import './Board.css';
 import Card from './Card';
 // import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
   constructor() {
@@ -13,13 +13,35 @@ class Board extends Component {
 
     this.state = {
       cards: [],
+      error: undefined,
     };
+  }
+
+  componentDidMount() {
+    const { url, boardName } = this.props;
+    const allCards = `${url}${boardName}/cards`;
+
+    axios.get(allCards)
+      .then((response) => {
+        const responseData = response.data;
+        console.log('successfully retrieved:', responseData);
+        this.setState({
+          cards: responseData,
+        })
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        console.log('error:', errorMessage);
+        this.setState({
+          error: errorMessage,
+        })
+      });
   }
 
   makeCardCollection = (cardData) => {
     return cardData.map((card, i) => {
       return <Card 
-        data={ card } 
+        data={ card.card } 
         key={ i }
       />
     });
@@ -28,7 +50,7 @@ class Board extends Component {
   render() {
     return (
       <div className="board">
-        {this.makeCardCollection(CARD_DATA.cards)}
+        {this.makeCardCollection(this.state.cards)}
       </div>
     )
   }
@@ -36,7 +58,8 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired,
 };
 
 export default Board;
