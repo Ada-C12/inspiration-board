f7784091f125e28a70129c09b0e1bb75211af050
eaball35 <eaball35@gmail.com>
2019-12-15 15:31:52 -0800

    added api, cards & styling

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,18 +8,49 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       cards: [],
+      error:'',
     };
   }
 
+  componentDidMount() {
+    axios.get(`${this.props.url}${this.props.boardName}/cards`)
+      .then((response) => {
+        const cards = response.data.map((obj) => {
+          return obj.card
+        });
+    
+        this.setState({
+          cards: cards
+        });
+        console.log(this.state.cards)
+      })
+      .catch((error) => {
+        this.setState({
+          error: 'Sorry, something went wrong'
+        });
+      });
+  }
+
   render() {
+    const cards = this.state.cards.map((card, i) => {
+      return <Card
+              id={card.id}
+              text={card.text}
+              emoji={card.emoji}
+            />;
+    });
+    
     return (
       <div>
-        Board
+        <h1 className='board-title'> {this.props.boardName} Board </h1>
+        <div className='board'>
+          {cards}
+        </div>
       </div>
     )
   }
@@ -27,7 +58,8 @@ class Board extends Component {
 }
 
 Board.propTypes = {
-
+  // url: PropTypes.string.isRequired,
+  // boardName: PropTypes.isRequired,
 };
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,20 @@ import './Card.css';
 class Card extends Component {
   render() {
     return (
-      <div className="card">
-        Card
+      <div className='card'>
+        <button type="button" class="card__delete">
+          Delete
+        </button>
+        
+        <section className="card__content">
+            <h2 className="card__content-text">
+              {this.props.text}
+            </h2>
+          
+          <div className='card__content-emoji'>
+            {this.props.emoji}
+          </div>
+        </section>
       </div>
     )
   }
