32209a2c3571b8ac8711fa81f9a264506fdd3274
gracexinran <gracexinran@gmail.com>
2019-12-15 16:03:21 -0800

    implemented delete card button

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,12 +5,19 @@ import emoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
+
+  deleteCard = (event) => {
+    event.preventDefault();
+    this.props.deleteCardCallback(this.props.id)
+  }
+
   render() {
     return (
       <div className="card">
         Card
         {this.props.text}
-        {this.props.emoji? emoji.getUnicode(this.props.emoji) : null}
+        {this.props.emoji ? emoji.getUnicode(this.props.emoji) : null}
+        <button onClick={this.deleteCard}>Delete Card</button>
       </div>
     )
   }
