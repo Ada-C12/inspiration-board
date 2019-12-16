60926d5b5a3c00a986b7aea05b39dfa1fe4a60a0
Morgan Schuler <morganlschuler@gmail.com>
2019-12-15 19:38:14 -0800

    updated card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,18 +4,32 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-class Card extends Component {
-  render() {
-    return (
-      <div className="card">
-        Card
+// class Card extends Component {
+//    render() {
+
+const Card = (props) => {
+  return (
+    <div className="card">
+  <section className='card__content'>
+  <section className='card__content-text' >{props.cardText} </section>
+  <section className='card__content-emoji'>{emoji.getUnicode(`${props.cardEmoji}`)}</section>
+  <button
+    type='button'
+    className='card__delete'
+    aria-label='Delete'
+    onClick={() => {props.deleteCardCallback(props.id)}}>
+      {console.log(props.id)}
+  Delete
+        </button>
+      </section>
       </div>
     )
   }
-}
 
 Card.propTypes = {
-
+  cardText: PropTypes.number.isRequired,
+  cardEmoji: PropTypes.string,
+  deleteCardCallback: PropTypes.func.isRequired
 };
 
 export default Card;
