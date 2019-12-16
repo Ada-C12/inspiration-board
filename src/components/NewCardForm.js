8eb06b337405480ce3dd604213f97d4b74bdf70e
Janice Huang <janicehuang@gmail.com>
2019-12-15 20:17:19 -0800

    add some more styling

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -49,17 +49,24 @@ class NewCardForm extends Component {
   render() {
     return (
       <form className="new-card-form" onSubmit={this.onFormSubmit}>
-        <div>
-          <label htmlFor="text">Text: </label>
+        <h3 className="new-card-form__header">Add Some Inspiration!</h3>
+        <div className="new-card-form__form">
+          <label className="new-card-form__form-label" htmlFor="text">
+            Text:
+          </label>
           <input
+            className="new-card-form__form-textarea"
             name="text"
             value={this.state.text}
             onChange={this.onTextChange}
           />
         </div>
-        <div>
-          <label htmlFor="curEmoji">Emoji: </label>
+        <div className="new-card-form__form">
+          <label className="new-card-form__form-label" htmlFor="curEmoji">
+            Emoji:
+          </label>
           <input
+            className="new-card-form__form-textarea"
             name="curEmoji"
             value={this.state.curEmoji}
             onChange={this.onEmojiChange}
