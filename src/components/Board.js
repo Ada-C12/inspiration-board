61bf4764c8f0251680db06b243a8b9642f634fe7
gracexinran <gracexinran@gmail.com>
2019-12-15 16:02:04 -0800

    get all cards through API call, add cards and delete cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+// import CARD_DATA from '../data/card-data.json';
 
 class Board extends Component {
   constructor() {
@@ -13,28 +13,73 @@ class Board extends Component {
 
     this.state = {
       cards: [],
+      error: undefined
     };
   }
 
+  componentDidMount() {
+    axios.get(this.props.url + this.props.boardName + '/cards')
+      .then((response) => {
+        this.setState({
+          cards: response.data
+        })
+      }).catch(() => {
+        this.setState({
+          error: 'Sorry! Sth went wrong.'
+        })
+      })
+  }
+
+  addCard = (card) => {
+    axios.post(this.props.url + this.props.boardName + '/cards', card)
+      .then((response) => {
+        const {cards} = this.state
+        cards.push(response.data)
+        this.setState({cards})
+      })
+      .catch(() => {
+        this.setState({
+          error: 'Sorry! Sth went wrong.'
+        })
+      }) 
+  }
+
+  deleteCard = (id) => {
+    axios.delete(`https://inspiration-board.herokuapp.com/cards/${id}`)
+      .then(() => {
+        const cards = this.state.cards.filter((card) => {
+          return card.card.id !== id
+        })
+        this.setState({cards})
+      })
+      .catch(() => {
+        this.setState({
+          error: 'Sorry! Sth went wrong.'
+        })
+      }) 
+  }
+
   render() {
-    const cardCollections = CARD_DATA.cards.map((card, i) => {
+    const cardCollections = this.state.cards.map((card, i) => {
       return (
         <Card
           key={i}
-          text={card.text}
-          emoji={card.emoji}
+          id={card.card.id}
+          text={card.card.text}
+          emoji={card.card.emoji}
+          deleteCardCallback={this.deleteCard}
         />
       )
     })
+
     return (
       <div>
         <h1>Board</h1>
-        {/* <NewCardForm /> */}
+        <NewCardForm addCardCallback={this.addCard}/>
         {cardCollections}
       </div>
     )
   }
-
 }
 
 Board.propTypes = {
