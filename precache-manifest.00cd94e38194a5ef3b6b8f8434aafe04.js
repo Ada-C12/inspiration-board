6c3f5b31de644e95d782170334880916483b4641
Rinostar <codingrinostar@gmail.com>
2019-12-18 21:48:31 -0800

    Updates

---
+++ precache-manifest.00cd94e38194a5ef3b6b8f8434aafe04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c23687f1ef11674f0a11d3181dfcfb",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "2b03f9a7e4022054b98c",
+    "url": "/inspiration-board/static/css/main.0b570879.chunk.css"
+  },
+  {
+    "revision": "9b99515a35e4013d7747",
+    "url": "/inspiration-board/static/js/2.d65b313a.chunk.js"
+  },
+  {
+    "revision": "098d96cd5327dd3fc6707a77e047f86b",
+    "url": "/inspiration-board/static/js/2.d65b313a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b03f9a7e4022054b98c",
+    "url": "/inspiration-board/static/js/main.aa0446e0.chunk.js"
+  },
+  {
+    "revision": "7b3d42590fd4530dd51b",
+    "url": "/inspiration-board/static/js/runtime-main.ecf8ceb5.js"
+  }
+]);
