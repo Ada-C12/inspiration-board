5b8298d44bf5c217691bf1e0e0ee4d02f059c39b
Sam <samcoll@Samanthas-MBP.localdomain>
2019-12-18 20:54:42 -0800

    created card class

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import emoji from 'emoji-dictionary';
-
+import * as cardEmoji from 'emoji-dictionary';
 import './Card.css';
 
 class Card extends Component {
-  render() {
-    return (
-      <div className="card">
-        Card
+
+render () {
+  const {id, text, emoji, deleteCardCallback} = this.props; 
+  return (
+      <div className="card card__content">
+        <p className="card__content-text">{id}</p>
+        <p className="card__content-text">{text}</p>
+        <p className="card__content-emoji">{cardEmoji.getUnicode(emoji)}</p>
+
+      <button className="card__delete"
+      onClick={() => {deleteCardCallback(id)}}>
+        Delete Card
+      </button>
       </div>
     )
   }
 }
 
 Card.propTypes = {
-
-};
+  id: PropTypes.number,
+  text: PropTypes.string,
+  emoji: PropTypes.string,
+}
 
 export default Card;
